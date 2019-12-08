ebc6a56a27da9d39f814dc877b44109cb65b0a4f
Drew Noakes <git@drewnoakes.com>
2019-12-09 08:48:12 +1100

    Update licenses

--- HexDump.Tests/HexDumpTests.cs
+++ HexDump.Tests/HexDumpTests.cs
@@ -1,3 +1,5 @@
+// Copyright (c) Drew Noakes. All Rights Reserved. Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
 using System;
 using System.Linq;
 using Xunit;
--- HexDump/HexDump.cs
+++ HexDump/HexDump.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Drew Noakes. All Rights Reserved. Licensed under the Apache License, Version 2.0. See LICENSE.md in the project root for license information.
+﻿// Copyright (c) Drew Noakes. All Rights Reserved. Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 using System;
 using System.Diagnostics.Contracts;
